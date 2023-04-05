8e8b244b4582255d9be060186cab86ce8bdd758d
2-6 <2-6@DESKTOP-VDBB49G>
2023-04-05 15:03:24 +0900

    ttets

---
+++ Project/src/controller/regServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import teamproject.Member;
+import teamproject.MemberDAO;
+
+@WebServlet("/reg")
+public class regServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/views/reg.jsp").forward(request, response);
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("uid");
+		String pw = request.getParameter("pw");
+		String tel = request.getParameter("tel");
+		String addr = request.getParameter("addr");
+		
+		System.out.println(name + " " + id + " " + pw + " " + tel + " " + addr);
+		
+		Member member = new Member(name, id, pw, tel, addr);
+		MemberDAO dao = new MemberDAO();
+		dao.insertMember(member);
+		
+		
+	}
+
+}
