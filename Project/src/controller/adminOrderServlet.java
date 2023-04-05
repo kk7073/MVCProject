a4219c246b0e64da3353026e77a5d23a82b37379
HyeriPark <119188681+HyeriPark95@users.noreply.github.com>
2023-04-05 13:21:33 +0900

    second

---
+++ Project/src/controller/adminOrderServlet.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/adminOrder")
+public class adminOrderServlet extends HttpServlet{
+	
+	
+}
